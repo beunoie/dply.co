56484ff4d2739c5ae5b0b42dbe4b2223afe607a7
Beunoie <bmailliet@laposte.net>
2018-04-21 21:20:16 +0200

    Create docker-mariadb-nginx.sh

---
+++ docker-mariadb-nginx.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+apt install git make
+git clone https://github.com/beunoie/debian-mariadb.git
+git clone https://github.com/beunoie/debian-nginx-php.git
+cd debian-mariadb
+make build
+make run
+cd ../debian-nginx-php/
+make build
+make prepare
+make run
+
+echo "All Done!"
